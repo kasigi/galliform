14419c2063be4773eef271090a41dfe7cfc161a6
Tor N. Johnson <kasigi@gmail.com>
2018-06-21 17:15:18 -0400

    added plugin bootstrap file

---
+++ galliform.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @package           galliform
+ * @wordpress-plugin
+ * Plugin Name:       Galliform QA Register
+ * Description:       Generate QA Checklists and Registers
+ * Version:           1.0.0
+ * Author:            Tor N. Johnson
+ * License:           GPL-3.0+
+ */
